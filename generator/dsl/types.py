c8d97c4aa3bb7a9fb590b350d101f4468c8bfed7
Jochem Rutgers <68805714+jhrutgers@users.noreply.github.com>
2020-06-18 18:36:21 +0200

    license

--- generator/dsl/types.py
+++ generator/dsl/types.py
@@ -1,4 +1,21 @@
 # vim:et
+
+# libstored, a Store for Embedded Debugger.
+# Copyright (C) 2020  Jochem Rutgers
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 import re
 import struct
 import copy
