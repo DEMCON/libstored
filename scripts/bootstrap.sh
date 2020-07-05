72a377cbb811d3e361a11129ff9dd13c06540bd4
Jochem Rutgers <68805714+jhrutgers@users.noreply.github.com>
2020-07-05 11:34:26 +0200

    osx

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -24,7 +24,18 @@ case `uname -s` in
 		fi
 		;;
 	Darwin*)
-		brew install cmake python3 pyqt
+		function install_or_upgrade {
+			if brew ls --versions "$1" >/dev/null; then
+				HOMEBREW_NO_AUTO_UPDATE=1 brew upgrade "$1"
+			else
+				HOMEBREW_NO_AUTO_UPDATE=1 brew install "$1"
+			fi
+		}
+		install_or_upgrade cmake
+		install_or_upgrade python3
+		install_or_upgrade pyqt
+		install_or_upgrade pkgconfig
+		install_or_upgrade gnutls
 		pip3 install jinja2 textx pyzmq pyside2 pyserial lognplot
 		;;
 	*)
