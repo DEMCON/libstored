2a0eda2b31aab99df25db910621f6d5e482a06cb
Jochem Rutgers <68805714+jhrutgers@users.noreply.github.com>
2020-12-03 17:09:03 +0100

    improve example

--- examples/9_fpga/main.cpp
+++ examples/9_fpga/main.cpp
@@ -1,3 +1,11 @@
+/*!
+ * \file
+ * \brief Example to show how to use libstored in VHDL.
+ *
+ * This is the C++ application, which can be used as a bridge between the VHDL
+ * simulation and a Debugger client, using named pipes.
+ */
+
 #include <stored>
 #include "ExampleFpga.h"
 #include "ExampleFpga2.h"
@@ -26,6 +34,10 @@ int main() {
 
 	stored::DebugZmqLayer zmq;
 	zmq.wrap(debugger);
+	if((errno = zmq.lastError())) {
+		perror("Cannot initialize ZMQ");
+		return 1;
+	}
 
 	stored::Synchronizer synchronizer;
 	synchronizer.map(exampleFpga);
@@ -45,13 +57,33 @@ int main() {
 	xsim.wrap(term);
 #endif
 
+	if((errno = xsim.lastError()) && errno != EAGAIN) {
+		perror("Cannot initialize XSIM interface");
+		return 1;
+	}
+
+	printf("\n"
+		"Start XSIM with the 9_fpga example. It connects to this application.\n"
+		"Use a Debugger client to see interaction with the VHDL simulation.\n");
+
 	stored::Poller poller;
 	poller.add(xsim, nullptr, stored::Poller::PollIn);
 	poller.add(xsim.req(), nullptr, stored::Poller::PollIn);
 	poller.add(zmq, nullptr, stored::Poller::PollIn);
 
 	while(true) {
-		poller.poll(1000000); // 1 s
+		// 1 s timeout, to force keep alives once in a while.
+		if(poller.poll(1000000).empty()) {
+			switch(errno) {
+			case EAGAIN:
+			case EINTR:
+				break;
+			default:
+				perror("poll failed");
+				return 1;
+			}
+		}
+
 		zmq.recv();
 		xsim.recv();
 
