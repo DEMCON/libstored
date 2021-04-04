6f77d7d11398fad48bd86070d255ca46a109021e
Jochem Rutgers <68805714+jhrutgers@users.noreply.github.com>
2021-04-05 00:21:11 +0200

    suppress warning

--- src/compress.cpp
+++ src/compress.cpp
@@ -199,5 +199,5 @@ bool CompressLayer::idle() const {
 
 } // namespace
 #else // !STORED_HAVE_HEATSHRINK
-char dummy_char_to_make_compress_cpp_non_empty;
+char dummy_char_to_make_compress_cpp_non_empty; // NOLINT
 #endif // STORED_HAVE_HEATSHRINK
