ad5f3e9759e1d293e8a69c004cd19679aa5fbc61
Jochem Rutgers <68805714+jhrutgers@users.noreply.github.com>
2021-08-25 00:10:51 +0200

    fixing build issues

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -20,7 +20,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), '../client'))
+sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), '../python'))
 import libstored
 
 import sphinx_rtd_theme
