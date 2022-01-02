0497a0f89a44a937cab871339966a5fdfc61af59
Jochem Rutgers <68805714+jhrutgers@users.noreply.github.com>
2022-01-02 22:58:35 +0100

    update copyright

--- tests/test_ZmqClient.py
+++ tests/test_ZmqClient.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # libstored, distributed debuggable data stores.
-# Copyright (C) 2020-2021  Jochem Rutgers
+# Copyright (C) 2020-2022  Jochem Rutgers
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Lesser General Public License as published by
