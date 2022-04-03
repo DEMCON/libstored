f13fbdb879b2feba802bcd0762452a4c77865bc8
Jochem Rutgers <68805714+jhrutgers@users.noreply.github.com>
2022-04-03 23:35:27 +0200

    older macos compatibility fixes

--- dist/macos/bootstrap.sh
+++ dist/macos/bootstrap.sh
@@ -16,20 +16,40 @@ function gotErr {
 
 trap gotErr ERR
 
-function brew_install {
-	if ! brew ls --versions "$1" >/dev/null; then
-		HOMEBREW_NO_AUTO_UPDATE=1 brew install "$1"
+if ! which brew > /dev/null && ! which port > /dev/null; then
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+function do_install {
+	if which brew > /dev/null; then
+		for p in "$@"; do
+			if ! brew ls --versions "$p" >/dev/null; then
+				HOMEBREW_NO_AUTO_UPDATE=1 brew install "$p" || gotErr
+			fi
+		done
+	elif which port > /dev/null; then
+		sudo port -N install "$@" || gotErr
+	else
+		echo "No brew or port found."
+		gotErr
 	fi
 }
 
-brew_install cmake
-brew_install python3
-brew_install pkgconfig
-brew_install gnutls
-brew_install doxygen
-brew_install plantuml
-brew_install ninja
-brew_install coreutils
-brew_install git
-brew_install zeromq
-brew_install jq
+do_install cmake pkgconfig gnutls doxygen plantuml ninja coreutils git jq
+
+if which brew > /dev/null; then
+	do_install zeromq
+elif which port > /dev/null; then
+	do_install zmq
+fi
+
+if which python3 > /dev/null; then
+	echo Skip install python3
+elif which brew > /dev/null; then
+	do_install python3
+elif which port > /dev/null; then
+	do_install python310
+	sudo port -N select --set python3 python310
+fi
+
+sudo python3 -m ensurepip
