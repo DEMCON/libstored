1cbbdfb266686541c89318b788e1529756befc00
Jochem Rutgers <68805714+jhrutgers@users.noreply.github.com>
2022-10-20 09:06:27 +0200

    tweaks for tidy

--- include/libstored/util.h
+++ include/libstored/util.h
@@ -140,6 +140,7 @@
  *
  * \param Class the class this macro is embedded in
  */
+// NOLINTBEGIN
 #		if STORED_cplusplus >= 201103L
 #			define STORED_CLASS_NOCOPY(Class)                      \
 			public:                                                 \
@@ -160,6 +161,7 @@
 				void operator=(Class const&);
 #		endif
 #	endif
+// NOLINTEND
 
 #	ifndef CLASS_NO_WEAK_VTABLE
 /*!
