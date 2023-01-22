e1260176d0d3d88c9a9d177f7f5e2dadfb3334f0
Jochem Rutgers <68805714+jhrutgers@users.noreply.github.com>
2023-01-22 23:56:08 +0100

    make list() thread-safe and optionally reentrant

--- src/directory.cpp
+++ src/directory.cpp
@@ -21,7 +21,7 @@ namespace stored {
  */
 static void
 list(void* container, void* buffer, uint8_t const* directory, ListCallbackArg* f, void* arg,
-     String::type& name)
+     String::type* name)
 {
 	if(unlikely(!buffer || !directory))
 		return;
@@ -41,16 +41,18 @@ list(void* container, void* buffer, uint8_t const* directory, ListCallbackArg* f
 			// NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast,performance-no-int-to-ptr)
 			char* b = Type::isFunction(type) ? reinterpret_cast<char*>(offset)
 							 : static_cast<char*>(buffer) + offset;
-			f(container, name.c_str(), type, b, len, arg);
+			f(container, name ? name->c_str() : nullptr, type, b, len, arg);
 			break;
 		} else if(*p <= 0x1f) {
 			// skip
-			name.append(*p, '?');
+			if(name)
+				name->append(*p, '?');
 			erase += *p;
 			p++;
 		} else if(*p == '/') {
 			// Hierarchy separator.
-			name.push_back('/');
+			if(name)
+				name->push_back('/');
 			erase++;
 			p++;
 		} else {
@@ -66,24 +68,26 @@ list(void* container, void* buffer, uint8_t const* directory, ListCallbackArg* f
 			list(container, buffer, p + jmp - 1, f, arg, name);
 
 			// resume with this char
-			name.push_back(c);
+			if(name)
+				name->push_back(c);
 			erase++;
 		}
 	}
 
-	if(erase)
-		name.erase(name.end() - (long)erase, name.end());
+	if(name && erase)
+		name->erase(name->end() - (long)erase, name->end());
 }
 
 /*!
- * \brief Iterates over all objects in the directory and invoke a callback for every object.
+ * \brief Iterates over all objects in the directory and invoke a callback for
+ *	every object.
  * \param container the container that contains \p buffer and \p directory
  * \param buffer the buffer that holds the data of all variables
  * \param directory the binary directory, to be parsed
  * \param f the callback function
  * \param arg an arbitrary argument to be passed to \p f
- * \param prefix when not \c nullptr, a string that is prepended for the name that is supplied to \p
- * f
+ * \param prefix when not \c nullptr, a string that is prepended for the name
+ *	that is supplied to \p f
  *
  * This function is not reentrant. Do not call it recursively.
  */
@@ -91,7 +95,7 @@ void list(
 	void* container, void* buffer, uint8_t const* directory, ListCallbackArg* f, void* arg,
 	char const* prefix)
 {
-	static String::type name;
+	static thread_local String::type name;
 	if(Config::AvoidDynamicMemory)
 		name.reserve(
 			(prefix ? strlen(prefix) * 2U : 0U)
@@ -100,7 +104,34 @@ void list(
 	if(prefix)
 		name = prefix;
 
-	list(container, buffer, directory, f, arg, name);
+	list(container, buffer, directory, f, arg, &name);
+}
+
+/*!
+ * \brief Iterates over all objects in the directory and invoke a callback for
+ *	every object.
+ * \param container the container that contains \p buffer and \p directory
+ * \param buffer the buffer that holds the data of all variables
+ * \param directory the binary directory, to be parsed
+ * \param nameBuffer use the provided buffer to assemble the object's names in.
+ *	Set to \c nullptr to skip name generation. In that case, \p f will
+ *	receive \c nullptr as name.
+ * \param f the callback function
+ * \param arg an arbitrary argument to be passed to \p f
+ * \param prefix when not \c nullptr, a string that is prepended for the name
+ *	that is supplied to \p f
+ *
+ * This function is not reentrant, unless \p nameBuffer is specified as \c
+ * nullptr.
+ */
+void list(
+	void* container, void* buffer, uint8_t const* directory, String::type* nameBuffer,
+	ListCallbackArg* f, void* arg, char const* prefix)
+{
+	if(nameBuffer && prefix)
+		*nameBuffer = prefix;
+
+	list(container, buffer, directory, f, arg, nameBuffer);
 }
 
 } // namespace stored
