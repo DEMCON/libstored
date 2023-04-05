22c75638136174666484cb99b585acd1bfeec828
Jochem Rutgers <68805714+jhrutgers@users.noreply.github.com>
2023-04-05 15:22:06 +0200

    bump year

--- include/libstored/poller.h
+++ include/libstored/poller.h
@@ -2,7 +2,7 @@
 #define LIBSTORED_POLLER_H
 /*
  * libstored, distributed debuggable data stores.
- * Copyright (C) 2020-2022  Jochem Rutgers
+ * Copyright (C) 2020-2023  Jochem Rutgers
  *
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
