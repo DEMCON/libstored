8a0850df609e7716d29435b6183cea568cf112a3
Jochem Rutgers <68805714+jhrutgers@users.noreply.github.com>
2023-08-25 16:23:17 +0200

    add protocol fuzzing

---
+++ tests/fuzz_common.cpp
@@ -0,0 +1,181 @@
+/*
+ * libstored, distributed debuggable data stores.
+ * Copyright (C) 2020-2023  Jochem Rutgers
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+#include "fuzz_common.h"
+#include "LoggingLayer.h"
+
+#include <algorithm>
+#include <cerrno>
+#include <cstdio>
+#include <cstring>
+#include <fcntl.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <vector>
+
+#ifdef HAVE_AFL
+__AFL_FUZZ_INIT();
+#endif
+
+void help(char const* exe)
+{
+	printf("Usage: %s [-h|-i|<input file>]\n", exe ? exe : fuzz_name);
+
+	printf("\nwhere\n");
+	printf("   -h   Show this help and exit.\n");
+	printf("   -i   Generate input files in the current directory and exit.\n");
+	printf("   <input file>\n");
+	printf("        The input file to run (without using AFL++).\n");
+
+	printf("\nWithout parameters, the program expects to be controlled by AFL++ for "
+	       "fuzzing.\n");
+}
+
+void generate(
+	std::initializer_list<char const*> msgs, stored::ProtocolLayer& top,
+	stored::ProtocolLayer& bottom)
+{
+	LoggingLayer l;
+	l.wrap(bottom);
+
+	for(auto const* msg : msgs) {
+		char len = (char)std::min<size_t>(0xff, strlen(msg));
+		top.encode(msg, (size_t)(uint8_t)len, true);
+		top.flush();
+		l.encoded().insert(
+			l.encoded().end() - 1, std::string{(char)l.encoded().back().size()});
+	}
+	auto buf = l.allEncoded();
+
+	static size_t count;
+	std::array<char, 64> filename{};
+	snprintf(filename.data(), filename.size(), "fuzz_%s_%03zu.bin", fuzz_name, count++);
+
+	FILE* f = fopen(filename.data(), "wb");
+	if(!f) {
+		printf("Cannot open %s for writing; %s\n", filename.data(), strerror(errno));
+		exit(1);
+	}
+
+	auto written = fwrite(buf.data(), buf.size(), 1, f);
+	fclose(f);
+
+	if(written != 1) {
+		printf("Cannot write %s\n", filename.data());
+		exit(1);
+	}
+
+	printf("Generated %s\n", filename.data());
+}
+
+void generate(std::initializer_list<char const*> msgs)
+{
+	stored::ProtocolLayer p;
+	generate(msgs, p, p);
+}
+
+void test(void const* buf, size_t len)
+{
+	test(Messages{buf, len});
+}
+
+int test(char const* file)
+{
+	int res = 1;
+	int fd = -1;
+	void* buf = nullptr;
+	size_t len = 0;
+	struct stat s {};
+
+	printf("Reading %s...\n", file);
+
+	fd = open(file, O_RDONLY);
+
+	if(fd == -1) {
+		printf("Cannot open file; %s\n", strerror(errno));
+		goto done;
+	}
+
+	if(fstat(fd, &s) == -1) {
+		printf("Cannot stat file; %s\n", strerror(errno));
+		goto close;
+	}
+
+	len = (size_t)s.st_size;
+	buf = mmap(nullptr, len, PROT_READ, MAP_SHARED, fd, 0);
+
+	if(buf == MAP_FAILED) {
+		printf("Cannot mmap; %s\n", strerror(errno));
+		goto close;
+	}
+
+	test({buf, len});
+	res = 0;
+
+	// unmap:
+	munmap(buf, len);
+close:
+	close(fd);
+done:
+	return res;
+}
+
+#ifdef HAVE_AFL
+#	ifdef STORED_COMPILER_CLANG
+#		pragma clang optimize off
+#	endif
+#	ifdef STORED_COMPILER_GCC
+#		pragma GCC optimize("O0")
+#	endif
+#endif
+int main(int argc, char** argv)
+{
+	printf("%s\n\n", stored::banner());
+	printf("Fuzzing %s\n\n", fuzz_name);
+
+	switch(argc) {
+	case 0:
+	case 1:
+		break;
+	case 2:
+		if(strcmp(argv[1], "-h") == 0) {
+			help(argv[0]);
+			return 0;
+		} else if(strcmp(argv[1], "-i") == 0) {
+			generate();
+			return 0;
+		}
+
+		return test(argv[1]);
+	default:
+		help(argv[0]);
+		return 1;
+	}
+
+#ifndef HAVE_AFL
+	printf("Compile this program with afl-clang-fast++ to do fuzzing.\n");
+	return 1;
+#else
+
+	printf("Ready. Waiting for afl-fuzz for instructions...\n");
+
+	__AFL_INIT();
+
+	unsigned char* buf = __AFL_FUZZ_TESTCASE_BUF;
+
+	while(__AFL_LOOP(1000000)) {
+		auto len = __AFL_FUZZ_TESTCASE_LEN;
+		test(buf, (size_t)len);
+	}
+
+	return 0;
+#endif
+}
--- tests/fuzz_debug.cpp
+++ tests/fuzz_debug.cpp
@@ -8,74 +8,14 @@
  */
 
 #include "TestStore.h"
+#include "fuzz_common.h"
 
-#include <algorithm>
-#include <cerrno>
-#include <cstdio>
 #include <cstring>
-#include <fcntl.h>
-#include <stored>
-#include <sys/mman.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <unistd.h>
 #include <vector>
 
-#ifdef __AFL_FUZZ_TESTCASE_LEN
-#	define HAVE_AFL
+char const* fuzz_name = "debug";
 
-#	pragma clang diagnostic ignored "-Wunused-but-set-variable"
-#	pragma clang diagnostic ignored "-Wshorten-64-to-32"
-
-__AFL_FUZZ_INIT();
-#endif
-
-static void help(char const* exe)
-{
-	printf("Usage: %s [-h|-i|<input file>]\n", exe);
-
-	printf("\nwhere\n");
-	printf("   -h   Show this help and exit.\n");
-	printf("   -i   Generate input files in the current directory and exit.\n");
-	printf("   <input file>\n");
-	printf("        The input file to run (without using AFL++).\n");
-
-	printf("\nWithout parameters, the program expects to be controlled by AFL++ for "
-	       "fuzzing.\n");
-}
-
-static void generate(std::initializer_list<char const*> msgs)
-{
-	std::string buf;
-
-	for(auto const* msg : msgs) {
-		size_t len = std::min<size_t>(0xff, strlen(msg));
-		buf.push_back((char)len);
-		buf.append(msg, len);
-	}
-
-	static size_t count;
-	std::array<char, 64> filename{};
-	snprintf(filename.data(), filename.size(), "fuzz_debug_%03zu.bin", count++);
-
-	FILE* f = fopen(filename.data(), "wb");
-	if(!f) {
-		printf("Cannot open %s for writing; %s\n", filename.data(), strerror(errno));
-		exit(1);
-	}
-
-	auto written = fwrite(buf.data(), buf.size(), 1, f);
-	fclose(f);
-
-	if(written != 1) {
-		printf("Cannot write %s\n", filename.data());
-		exit(1);
-	}
-
-	printf("Generated %s\n", filename.data());
-}
-
-static void generate()
+void generate()
 {
 	generate({"?", "i", "v", "l"});
 	generate({"r/default int8", "a1/default int8", "r1", "w101", "r1"});
@@ -84,117 +24,17 @@ static void generate()
 	generate({"mt|r/default uint32", "ttt", "st", "t"});
 }
 
-static void test(void const* buf, size_t len)
+void test(Messages const& msgs)
 {
-	static std::vector<uint8_t> msg;
+	std::vector<uint8_t> buf;
 
 	stored::TestStore store;
 	stored::Debugger debugger;
 	debugger.map(store);
 
-	// buf contains messages, which are a byte with the length and then one message of that
-	// length.
-	uint8_t const* buf_ = static_cast<uint8_t const*>(buf);
-
-	while(len > 0) {
-		size_t l = std::min<size_t>(--len, *buf_++);
-
-		msg.resize(l);
-		memcpy(msg.data(), buf_, l);
-		debugger.decode(msg.data(), l);
-
-		buf_ += l;
-		len -= l;
+	for(auto const& msg : msgs) {
+		buf.resize(msg.size());
+		memcpy(buf.data(), msg.data(), msg.size());
+		debugger.decode(buf.data(), msg.size());
 	}
-}
-
-static int test(char const* file)
-{
-	int res = 1;
-	int fd = -1;
-	void* buf = nullptr;
-	size_t len = 0;
-	struct stat s {};
-
-	printf("Reading %s...\n", file);
-
-	fd = open(file, O_RDONLY);
-
-	if(fd == -1) {
-		printf("Cannot open file; %s\n", strerror(errno));
-		goto done;
-	}
-
-	if(fstat(fd, &s) == -1) {
-		printf("Cannot stat file; %s\n", strerror(errno));
-		goto close;
-	}
-
-	len = (size_t)s.st_size;
-	buf = mmap(nullptr, len, PROT_READ, MAP_SHARED, fd, 0);
-
-	if(buf == MAP_FAILED) {
-		printf("Cannot mmap; %s\n", strerror(errno));
-		goto close;
-	}
-
-	test(buf, len);
-	res = 0;
-
-	// unmap:
-	munmap(buf, len);
-close:
-	close(fd);
-done:
-	return res;
-}
-
-#ifdef HAVE_AFL
-#	ifdef STORED_COMPILER_CLANG
-#		pragma clang optimize off
-#	endif
-#	ifdef STORED_COMPILER_GCC
-#		pragma GCC optimize("O0")
-#	endif
-#endif
-int main(int argc, char** argv)
-{
-	printf("%s\n\n", stored::banner());
-	printf("Debug fuzzer\n\n");
-
-	switch(argc) {
-	case 0:
-	case 1:
-		break;
-	case 2:
-		if(strcmp(argv[1], "-h") == 0) {
-			help(argv[0]);
-			return 0;
-		} else if(strcmp(argv[1], "-i") == 0) {
-			generate();
-			return 0;
-		}
-
-		return test(argv[1]);
-	default:
-		help(argv[0]);
-		return 1;
-	}
-
-#ifndef HAVE_AFL
-	printf("Compile this program with afl-clang-fast++ to do fuzzing.\n");
-	return 1;
-#else
-
-	printf("Ready. Waiting for afl-fuzz for instructions...\n");
-
-	__AFL_INIT();
-
-	unsigned char* buf = __AFL_FUZZ_TESTCASE_BUF;
-
-	while(__AFL_LOOP(1000000)) {
-		auto len = __AFL_FUZZ_TESTCASE_LEN;
-		test(buf, (size_t)len);
-	}
-#endif
 }
