9d23d1a99af28833141300f431bd569f6d6992b2
Jochem Rutgers <68805714+jhrutgers@users.noreply.github.com>
2023-11-09 17:00:26 +0100

    allow specifying python for venv

--- dist/common/venv.sh
+++ dist/common/venv.sh
@@ -11,11 +11,56 @@ function gotErr {
 	exit 2
 }
 
+function venv_help {
+	echo "Usage:"
+	echo "    $0 [-h] [-p <python_exe>] [install|check|clean]"
+	echo "    source $0 activate|deactivate"
+	echo ""
+	echo "where"
+	echo "  -h   Show this help"
+	echo "  -p   Use given python executable. Default: ${python}"
+	return 2
+}
+
+python=`which python3`
+
+if which greadlink > /dev/null; then
+	# For macOS, with coreutils
+	readlink=greadlink
+else
+	readlink=readlink
+fi
+
+while getopts "hp:" arg; do
+	case "${arg}" in
+		h)
+			venv_help
+			exit 0
+			;;
+		p)
+			python=`which "${OPTARG}"`
+			if [[ -z ${python} ]]; then
+				echo "Cannot find python executable '${OPTARG}'" >&2
+				exit 2
+			fi
+			;;
+		*)
+			venv_help
+			exit 2
+	esac
+done
+shift $((OPTIND-1))
+
+if [[ ! -x ${python} ]]; then
+	echo "Cannot find python executable '${python}'" >&2
+	exit 2
+fi
+
 dir="$( cd "$(dirname "${BASH_SOURCE[0]}")"/..; pwd -P )"
 venv_dir="${dir}/venv"
 
 in_venv=0
-python3 "${dir}/common/check_venv.py" || in_venv=1
+"${python}" "${dir}/common/check_venv.py" || in_venv=1
 
 function venv_install {
 	if [[ ! -e ${venv_dir} ]]; then
@@ -26,12 +71,12 @@ function venv_install {
 		fi
 
 		if [[ ${in_venv} == 1 ]]; then
-			echo "Preparing current venv with `which python3`..."
-			python3 -m pip install --upgrade ${pkg} || gotErr
+			echo "Preparing current venv with ${python}..."
+			"${python}" -m pip install --upgrade ${pkg} || gotErr
 			mkdir -p "${venv_dir}" || gotErr
 		else
 			echo Installing venv in "${venv_dir}"...
-			python3 -m venv "${venv_dir}" || gotErr
+			"${python}" -m venv "${venv_dir}" || gotErr
 			"${venv_dir}/bin/python3" -m pip install --upgrade ${pkg} || gotErr
 		fi
 	fi
@@ -44,23 +89,21 @@ function venv_just_activate {
 		set +u
 		source "${venv_dir}/bin/activate"
 		set -u
-		if which greadlink > /dev/null; then
-			# For macOS, with coreutils
-			readlink=greadlink
-		else
-			readlink=readlink
-		fi
+
 		if [[ `${readlink} -f \`which python3\`` != `${readlink} -f ${venv_dir}/bin/python3` ]]; then
 			echo "activate failed" >&2
 			return 2
 		fi
+
+		# Use venv's python3 from now on.
+		python=`which python3`
 	fi
 }
 
 function venv_requirements_minimal {
 	venv_just_activate
-	echo "Installing minimal dependencies in "${venv_dir}" using `which python3`..."
-	python3 -m pip install --prefer-binary --upgrade \
+	echo "Installing minimal dependencies in "${venv_dir}" using ${python}..."
+	"${python}" -m pip install --prefer-binary --upgrade \
 		-r "${dir}/common/requirements-minimal.txt" || gotErr
 
 	touch "${venv_dir}/.timestamp-minimal" || gotErr
@@ -68,8 +111,8 @@ function venv_requirements_minimal {
 
 function venv_requirements {
 	venv_just_activate
-	echo "Installing dependencies in "${venv_dir}" using `which python3`..."
-	python3 -m pip install --prefer-binary --upgrade \
+	echo "Installing dependencies in "${venv_dir}" using ${python}..."
+	"${python}" -m pip install --prefer-binary --upgrade \
 		-r "${dir}/common/requirements.txt" || gotErr
 
 	touch "${venv_dir}/.timestamp-minimal" || gotErr
@@ -124,17 +167,15 @@ function venv_clean {
 	fi
 }
 
-function venv_help {
-	echo "Usage:"
-	echo "    $0 install|check|clean"
-	echo "    source $0 activate|deactivate"
-	return 2
-}
-
 case "$#" in
-	0) op="check";;
-	1) op="$1";;
-	*) venv_help;;
+	0)
+		op="check";;
+	1)
+		op="$1";;
+	*)
+		venv_help
+		exit 2
+		;;
 esac
 
 case "${op}" in
