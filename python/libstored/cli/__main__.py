1817ef9bfff3138594f3072335e120a916564e59
Jochem Rutgers <68805714+jhrutgers@users.noreply.github.com>
2025-10-21 13:57:18 +0200

    migrate cli

--- python/libstored/cli/__main__.py
+++ python/libstored/cli/__main__.py
@@ -1,44 +1,57 @@
 #!/usr/bin/env python3
 
-# SPDX-FileCopyrightText: 2020-2023 Jochem Rutgers
+# SPDX-FileCopyrightText: 2020-2025 Jochem Rutgers
 #
 # SPDX-License-Identifier: MPL-2.0
 
-import sys
+import aiofiles
 import argparse
 import logging
+import os
 
-from ..zmq_client import ZmqClient
+from ..asyncio.zmq_client import ZmqClient
+from ..asyncio.worker import run_sync, AsyncioWorker
 from ..zmq_server import ZmqServer
+from ..version import __version__
+
+@run_sync
+async def async_main(args : argparse.Namespace):
+    async with ZmqClient(args.server, args.port, multi=True) as client:
+        prefix = '>  '
+        await aiofiles.stdout.write(prefix)
+        await aiofiles.stdout.flush()
+        async for line in aiofiles.stdin:
+            line = line.strip()
+            if len(line) > 0:
+                await aiofiles.stdout.write('<  ' + await client.req(line) + '\n')
+            await aiofiles.stdout.write(prefix)
+            await aiofiles.stdout.flush()
 
 def main():
     parser = argparse.ArgumentParser(description='ZMQ command line client')
+    parser.add_argument('-V', action='version', version=__version__)
     parser.add_argument('-s', dest='server', type=str, default='localhost', help='ZMQ server to connect to')
     parser.add_argument('-p', dest='port', type=int, default=ZmqServer.default_port, help='port')
-    parser.add_argument('-v', dest='verbose', default=False, help='Enable verbose output', action='store_true')
+    parser.add_argument('-v', dest='verbose', default=0, help='Enable verbose output', action='count')
 
     args = parser.parse_args()
 
-    if args.verbose:
+    if args.verbose == 0:
+        logging.basicConfig(level=logging.WARN)
+    elif args.verbose == 1:
+        logging.basicConfig(level=logging.INFO)
+    else:
         logging.basicConfig(level=logging.DEBUG)
 
-    client = ZmqClient(args.server, args.port, multi=True)
+    with AsyncioWorker() as w:
+        try:
+            async_main(args)
+        except KeyboardInterrupt:
+            pass
+        finally:
+            w.cancel()
 
-    try:
-        prefix = '>  '
-        sys.stdout.write(prefix)
-        sys.stdout.flush()
-        for line in sys.stdin:
-            line = line.strip()
-            if len(line) > 0:
-                print('<  ' + client.req(line.encode()).decode())
-            sys.stdout.write(prefix)
-            sys.stdout.flush()
-
-    except KeyboardInterrupt:
-        pass
-
-    client.close()
+    os._exit(0)
 
 if __name__ == '__main__':
     main()
