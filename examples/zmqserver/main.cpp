218a47ec356ea6ff5c490f518a5648b9f08074d8
Jochem Rutgers <68805714+jhrutgers@users.noreply.github.com>
2025-10-28 12:58:23 +0100

    fix

--- examples/zmqserver/main.cpp
+++ examples/zmqserver/main.cpp
@@ -43,7 +43,7 @@ protected:
 	void __compute__length_of__a_string(bool set, uint32_t& value)
 	{
 		if(!set) {
-			auto v = a_string.variant();
+			decltype(a_string.variant()) v = a_string.variant();
 			v.entryRO();
 			value = (uint32_t)strlen(static_cast<char*>(v.buffer()));
 			v.exitRO();
@@ -162,7 +162,8 @@ int main()
 #elif defined(STORED_OS_WINDOWS)
 			Sleep(sleep_ms);
 #else
-			nanosleep(sleep_ms * 1000000ULL);
+			struct timespec ts = {sleep_ms / 1000, (long)(sleep_ms % 1000) * 1000000L};
+			nanosleep(&ts, NULL);
 #endif
 
 			if((errno = zmqLayer.recv())) {
